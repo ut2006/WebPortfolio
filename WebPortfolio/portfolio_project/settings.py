86f0028396235dc255d7dc60ceb2896f57bbf76f
ut2006 <hoangthu2k6@gmail.com>
2026-06-12 23:12:10 +0700

    Fix production settings

--- WebPortfolio/portfolio_project/settings.py
+++ WebPortfolio/portfolio_project/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-abc123xyz'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
