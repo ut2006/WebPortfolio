59b01f1cb619f32b238e756b18b4c6900422fec1
ut2006 <hoangthu2k6@gmail.com>
2026-06-12 23:24:22 +0700

    Fix static and media

--- WebPortfolio/portfolio_project/urls.py
+++ WebPortfolio/portfolio_project/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
